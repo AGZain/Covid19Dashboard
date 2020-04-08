8efe32f40df03f8dbb6a2b3fa1bf5668c1456497
Abdul <abdulzainulabedin@gmail.com>
2020-04-08 19:18:23 -0400

    Updates

---
+++ precache-manifest.5811e56fbf574821d9135a27afae3af3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c9af2e9204288edb4ad494fb188741",
+    "url": "/covid19dashboard/index.html"
+  },
+  {
+    "revision": "192f85dc6350c32a3882",
+    "url": "/covid19dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c90f7e75ad1263cbc06",
+    "url": "/covid19dashboard/static/js/2.5cb95793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19dashboard/static/js/2.5cb95793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192f85dc6350c32a3882",
+    "url": "/covid19dashboard/static/js/main.85f1a98f.chunk.js"
+  },
+  {
+    "revision": "92de254e8f58963d0dd1",
+    "url": "/covid19dashboard/static/js/runtime-main.6cc6d0f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
