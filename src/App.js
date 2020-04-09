a0ff88b1baf9031535eb1ad7af4f5034826bd2ce
Abdul <abdulzainulabedin@gmail.com>
2020-04-08 23:28:01 -0400

    testing out recharts

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
             <code>src/App.js</code> 
           </p>
         </header>
-        {/* <ScatterChart
+        <ScatterChart
           width={600}
           height={400}
           margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
@@ -56,7 +56,7 @@ class App extends Component {
           <Tooltip cursor={{ strokeDasharray: "3 3" }} />
           <Legend />
           <Scatter name="A school" data={this.state.list} fill="#8884d8" line shape="line" />
-        </ScatterChart> */}
+        </ScatterChart>
       </div>
     );
   }
